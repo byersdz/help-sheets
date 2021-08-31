a55b41fba4aa605f9e526549a02b45be1be9365d
David Byers <byersdz@gmail.com>
2021-08-31 17:21:20 -0400

    format 11 digit phone numbers

--- src/formatters/formatPhone.js
+++ src/formatters/formatPhone.js
@@ -1,7 +1,12 @@
 function formatPhone( phone ) {
   let result = phone;
 
-  if ( phone.length === 10 ) {
+  if ( phone.length === 11 ) {
+    result = `${ phone.slice( 0, 1 ) } `;
+    result += `(${ phone.slice( 1, 4 ) }) `;
+    result += `${ phone.slice( 4, 7 ) }-${ phone.slice( 7 ) }`;
+  }
+  else if ( phone.length === 10 ) {
     result = `(${ phone.slice( 0, 3 ) }) ${ phone.slice( 3, 6 ) }-${ phone.slice( 6 ) }`;
   }
 
