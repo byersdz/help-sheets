01103ac721b9db40dae93460150cd92a388f054d
David Byers <byersdz@gmail.com>
2021-09-02 22:28:34 -0400

    add call to 3 digit phone numbers

--- src/formatters/formatPhone.js
+++ src/formatters/formatPhone.js
@@ -9,6 +9,9 @@ function formatPhone( phone ) {
   else if ( phone.length === 10 ) {
     result = `(${ phone.slice( 0, 3 ) }) ${ phone.slice( 3, 6 ) }-${ phone.slice( 6 ) }`;
   }
+  else if ( phone.length === 3 ) {
+    result = `call ${ phone }`;
+  }
 
   return result;
 }
