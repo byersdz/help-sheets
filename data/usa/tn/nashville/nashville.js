df3c0e08449bf5a1c2834d4ff4cbc32216bbbeac
David Byers <byersdz@gmail.com>
2023-12-08 19:53:08 -0500

    add nashville rescue mission

--- data/usa/tn/nashville/nashville.js
+++ data/usa/tn/nashville/nashville.js
@@ -12,18 +12,6 @@ data[keys.EXCLUDE_LIST] = [
 ];
 
 data[keys.ACCESS_POINTS] = [
-  {
-    [keys.NAME]: "Coordinated Shelter Access",
-    [keys.URL]: "https://louhomeless.org/news/gethelp",
-    [keys.PHONE]: "5026372337",
-    [keys.DATE_CHECKED]: "2022-07-19",
-    [keys.DESCRIPTION]: "Call if you are homeless and in need of emergency shelter.",
-    [keys.ADDRESS_1]: "1300 S. 4th Street",
-    [keys.ADDRESS_2]: "#250",
-    [keys.CITY]: "Louisville",
-    [keys.STATE]: "KY",
-    [keys.ZIP_CODE]: "40208",
-  },
 ];
 
 data[keys.BASIC_NEEDS] = [
@@ -51,15 +39,15 @@ data[keys.BASIC_NEEDS] = [
 
 data[keys.EMERGENCY_SHELTERS] = [
   {
-    [keys.NAME]: "Wayside Christian Mission",
-    [keys.URL]: "https://www.waysidechristianmission.org/ways-to-get-help",
-    [keys.PHONE]: "5025843711",
-    [keys.DATE_CHECKED]: "2023-08-15",
-    [keys.ADDRESS_1]: "432 East Jefferson Street St",
-    [keys.CITY]: "Louisville",
-    [keys.STATE]: "KY",
-    [keys.ZIP_CODE]: "40202",
-    [keys.SERVES]: [serves.MEN, serves.WOMEN],
+    [keys.NAME]: "Nashville Rescue Mission",
+    [keys.URL]: "https://nashvillerescuemission.org",
+    [keys.PHONE]: "6152552475",
+    [keys.DATE_CHECKED]: "2023-12-08",
+    [keys.ADDRESS_1]: "639 Lafayette Street",
+    [keys.CITY]: "Nashville",
+    [keys.STATE]: "TN",
+    [keys.ZIP_CODE]: "37203",
+    [keys.SERVES]: [serves.MEN],
   },
 ];
 
