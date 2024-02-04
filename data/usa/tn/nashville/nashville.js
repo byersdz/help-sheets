fa39612bddb1553364a09af437be08885bdd9334
David Byers <byersdz@gmail.com>
2024-02-03 19:33:49 -0500

    add community care fellowship

--- data/usa/tn/nashville/nashville.js
+++ data/usa/tn/nashville/nashville.js
@@ -16,6 +16,31 @@ data[keys.ACCESS_POINTS] = [
 
 data[keys.BASIC_NEEDS] = [
   {
+    [keys.NAME]: "Community Care Fellowship",
+    [keys.URL]: "https://www.ccfnashville.org/get-help",
+    [keys.PHONE]: "6152271953",
+    [keys.DATE_CHECKED]: "2024-02-03",
+    [keys.ADDRESS_1]: "511 South 8th Street",
+    [keys.CITY]: "Nashville",
+    [keys.STATE]: "TN",
+    [keys.ZIP_CODE]: "37206",
+    [keys.DESCRIPTION]: "Also provides showers, laundry, mail, phone, wifi",
+    [keys.PROVIDES]: [
+      {
+        [keys.DESCRIPTION]: "Breakfast",
+        [keys.HOURS]: "Mon-Fri 9am - 9:30am",
+      },
+      {
+        [keys.DESCRIPTION]: "Lunch",
+        [keys.HOURS]: "Tue/Thur/Fri 11:30am - 12:30pm",
+      },
+      {
+        [keys.DESCRIPTION]: "Clothing",
+        [keys.HOURS]: "Mon-Fri 1pm - 3pm",
+      },
+    ],
+  },
+  {
     [keys.NAME]: "Harvest Sound",
     [keys.URL]: "https://harvestsoundinternational.com",
     [keys.PHONE]: "6153271200",
