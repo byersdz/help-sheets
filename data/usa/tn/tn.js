289ce14e257ab1c0b3c4d9b64df050ee26255ed0
David Byers <byersdz@gmail.com>
2024-03-29 21:34:25 -0400

    add tennessee redline

--- data/usa/tn/tn.js
+++ data/usa/tn/tn.js
@@ -15,4 +15,15 @@ data[keys.EXCLUDE_LIST] = [];
 
 data[keys.ACCESS_POINTS] = [];
 
+data[keys.RESOURCES] = [
+  {
+    [keys.NAME]: "Tennessee Redline",
+    [keys.URL]: "https://www.taadas.org/tn-redline",
+    [keys.PHONE]: "8008899789",
+    [keys.DATE_CHECKED]: "2024-03-29",
+    [keys.DESCRIPTION]:
+      "Connects Tennessee residents with addiction treatment and recovery services.",
+  },
+];
+
 module.exports = data;
