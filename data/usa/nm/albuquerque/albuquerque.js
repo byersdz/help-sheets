fd459cda9c5db34975bcf7facc57ba05eb3d1501
David Byers <byersdz@gmail.com>
2025-12-08 17:37:06 -0500

    add hopeworks

--- data/usa/nm/albuquerque/albuquerque.js
+++ data/usa/nm/albuquerque/albuquerque.js
@@ -16,22 +16,23 @@ data[keys.ACCESS_POINTS] = [
 
 data[keys.BASIC_NEEDS] = [
   {
-    [keys.NAME]: "Jesus House",
-    [keys.URL]: "https://jesushouseokc.org/our-programs",
-    [keys.PHONE]: "4052327164",
-    [keys.DATE_CHECKED]: "2025-11-07",
-    [keys.ADDRESS_1]: "1335 West Sheridan Ave",
-    [keys.CITY]: "Oklahoma City",
-    [keys.STATE]: "OK",
-    [keys.ZIP_CODE]: "73106",
+    [keys.NAME]: "HopeWorks",
+    [keys.URL]: "https://hopeworksnm.org/get-help",
+    [keys.PHONE]: "5058439405",
+    [keys.DATE_CHECKED]: "2025-12-08",
+    [keys.ADDRESS_1]: "1201 3rd St NW",
+    [keys.CITY]: "Albuquerque",
+    [keys.STATE]: "NM",
+    [keys.ZIP_CODE]: "87102",
+    [keys.DESCRIPTION]: "Day shelter, clothing",
     [keys.PROVIDES]: [
       {
-        [keys.DESCRIPTION]: "Dinner",
-        [keys.HOURS]: "Every day 4pm-5pm",
+        [keys.DESCRIPTION]: "Coffee & meal checkin",
+        [keys.HOURS]: "Mon-Fri 8am-10am",
       },
       {
-        [keys.DESCRIPTION]: "Food Pantry",
-        [keys.HOURS]: "Wednesday 8am",
+        [keys.DESCRIPTION]: "Lunch",
+        [keys.HOURS]: "Mon-Fri 10am-noon",
       },
     ],
   },
